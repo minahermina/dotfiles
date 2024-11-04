4f87162471f1cf60966c526084968f43606b5428
minaalbertsaeed <mina.albert33@gmail.com>
2024-11-04 06:11:33 +0200

    Edit dwm colors

--- src/dwm/config.h
+++ src/dwm/config.h
@@ -24,7 +24,7 @@ static const char col_gray1[]       = "#181818";
 static const char col_gray2[]       = "#555555"; 
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#008cc4";
+static const char col_cyan[]        = "#cccccc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	// [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
