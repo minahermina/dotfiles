72d2569fdb2c86c057fd082db9a945330765c747
minaalbertsaeed <mina.albert33@gmail.com>
2024-12-29 05:50:30 +0200

    Edit screencast.sh to use Nvidia GPU

--- .local/scripts/screencast.sh
+++ .local/scripts/screencast.sh
@@ -9,26 +9,28 @@ output_file="$HOME/hdd/D/Mina/recordings/screencast_$(date +"%Y_%m_%d_%H_%M_%S")
 start_recording() {
     # Use slop to get the geometry for the region to be recorded
     geometry=$(slop -f "%x %y %w %h")
-    
+
     # Parse the geometry
     read -r x y width height <<< "$geometry"
 
     echo "Starting recording..."
-    
+
     # Run ffmpeg with the selected region
-    ffmpeg -f x11grab -video_size "${width}x${height}" -i :0.0+$x,$y \
-           -f pulse -ac 2 -i alsa_input.pci-0000_05_00.6.analog-stereo \
-           "$output_file" &
-    pkill -RTMIN+22 dwmblocks
-}
+    ffmpeg -f x11grab -framerate 120 -video_size "${width}x${height}" -i :0.0+$x,$y \
+        -f pulse -ac 2 -i alsa_input.pci-0000_05_00.6.monitor \
+        -c:v h264_nvenc -preset fast -qp 23 \
+        -c:a aac -b:a 192k \
+        "$output_file" &
+            pkill -RTMIN+22 dwmblocks
+        }
 
-stop_recording() {
-    echo "Stopping recording..."
-    pkill ffmpeg
-    pkill dwmblocks && dwmblocks & disown
-}
+        stop_recording() {
+            echo "Stopping recording..."
+            pkill ffmpeg
+            pkill dwmblocks && dwmblocks & disown
+        }
 
-process_name="ffmpeg"
+        process_name="ffmpeg"
 
 # Check if ffmpeg is already running
 if pgrep -x "$process_name" > /dev/null; then
