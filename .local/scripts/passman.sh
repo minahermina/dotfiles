aa8639a492bc23b321b8d78094768a3415ed7f38
minahermina <mina.albert.saeed@gmail.com>
2025-05-15 00:05:04 +0300

    Add passman.sh

---
+++ .local/scripts/passman.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+shopt -s nullglob globstar
+
+# prompt list of passwords
+dmenu="dmenu \-p \"Choose a pass:\" -l 10 "
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+# get user choice of password
+password=$(printf '%s\n' "${password_files[@]}" | eval "$dmenu" )
+[ -z "$password" ] && exit
+
+# get password info from pass program
+password_info=$(pass "$password")
+
+password_info_fields=$(echo "$password_info" | awk 'NR==1 {print "password" } NR>1 {print $1}' | sed 's/://')
+
+chosen_field=$(echo "$password_info_fields" | eval "$dmenu")
+[ -z "$chosen_field" ] && exit
+
+result=$(pass show "$password" | grep "$chosen_field" | sed "s/$chosen_field: //")
+
+# password field is chosen
+if [ -z "$result" ]; then
+   result=$(pass show "$password" | awk 'NR==1')
+fi
+
+echo "$result" | xclip -sel clip
