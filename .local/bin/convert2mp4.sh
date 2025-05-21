853864a55b78950ad4c4f0106f1cadeb9f91fa9f
minahermina <mina.albert.saeed@gmail.com>
2025-05-21 06:40:19 +0300

    Delete stuff from .local/bin

--- .local/bin/convert2mp4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-infile=$1
-printf "Enter the file name:" 
-read outfile
-ffmpeg -i "$infile" -vcodec libx264 -acodec aac -movflags +faststart -preset fast -crf 28 -vf "scale=640:-2" "$outfile"
-
